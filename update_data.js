5930f62efd210d823851d977b3dd50328d10c851
alimpalla <ali.m.palla@gmail.com>
2022-01-26 19:20:07 -0800

    Update update_data.js

--- update_data.js
+++ update_data.js
@@ -47,34 +47,34 @@ function formatAMPM(date) {
 
 // 2. Update Prayer Time
 var saved_prayer = "";
-function update_times(times_list) {
+function update_times(times_dict) {
   $(() => {
 
     // times input as list from fetch_data function
 
     // send each time to matching element in window
     // not sending current prayer, that varies so is dealt with later
-    $("#fajrTime").text(times_list[0]);
-    $("#dhuhrTime").text(times_list[1])
-    $("#asrTime").text(times_list[2])
-    $("#maghribTime").text(times_list[3])
-    $("#ishaTime").text(times_list[4])
-    $("#currentTime").text(times_list[6])
-    $("#locationName").text(times_list[7])
-    $("#nextPrayer").text(times_list[8])
-    $("#timeToNext").text(times_list[9])
+    $("#fajrTime").text(times_dict.fajr_time)
+    $("#dhuhrTime").text(times_dict.dhuhr_time)
+    $("#asrTime").text(times_dict.asr_time)
+    $("#maghribTime").text(times_dict.maghrib_time)
+    $("#ishaTime").text(times_dict.isha_time)
+    $("#currentTime").text(times_dict.current_time)
+    $("#locationName").text(times_dict.location)
+    $("#nextPrayer").text(times_dict.next_prayer)
+    $("#timeToNext").text(times_dict.time_to_next)
 
     // sends current prayer text based on if between sunrise and dhuhr or not
-    if(times_list[5] === ("Sunrise")) {
+    if(times_dict.current_prayer === ("Sunrise")) {
       // fill current prayer text "It's after sunrise"
-      $("#currentPrayer").text("It's After ".concat(times_list[5]));
+      $("#currentPrayer").text("It's After ".concat(times_dict.current_prayer));
     } else { 
       // fill current prayer text "It's currently [insert prayer]"
-      $("#currentPrayer").text("It's Currently ".concat(times_list[5]));
+      $("#currentPrayer").text("It's Currently ".concat(times_dict.current_prayer));
     }
 
     // change highlighting when prayer changes
-    if (times_list[5] !== saved_prayer) {
+    if (times_dict.current_prayer !== saved_prayer) {
 
       // remove all highlighting of current prayer
       // catches errors that emerge here
@@ -86,11 +86,11 @@ function update_times(times_list) {
       }
 
       // change saved prayer to current
-      saved_prayer = times_list[5];
+      saved_prayer = times_dict.current_prayer;
       
       // assign to current if not between sunrise and dhuhr
-      if(times_list[5] !== ("Sunrise")) {
-        document.getElementById(times_list[5]).classList.add("currentPrayerColumn");
+      if(times_dict.current_prayer !== ("Sunrise")) {
+        document.getElementById(times_dict.current_prayer).classList.add("currentPrayerColumn");
       } 
     }
 
@@ -101,23 +101,23 @@ function update_times(times_list) {
 // Current Location http://127.0.0.1:5000/return_times --> GET
 // https://alipalla.pythonanywhere.com/return_times
 function fetchData() {
-    axios.get('https://alipalla.pythonanywhere.com/return_times')
-    .then(function (response) {
-        // handle success - display returned data
-        // run other functions within this .then 
-        //     due to async
-        //console.log(response);
-        output_str = response.data;
-        //console.log(output_str);
-        update_times(output_str.split(';'))
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    })
-    .then(function () {
-        // always executed
-    });
+  axios.get('https://alipalla.pythonanywhere.com/return_times')
+  .then(function (response) {
+      // handle success - display returned data
+      // run other functions within this .then 
+      //     due to async
+      //console.log(response);
+      times_dict = response.data;
+      //console.log(times_dict.fajr_time);
+      update_times(times_dict)
+  })
+  .catch(function (error) {
+      // handle error
+      console.log(error);
+  })
+  .then(function () {
+      // always executed
+  });
 }
 
 fetchData()
